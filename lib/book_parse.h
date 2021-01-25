2730ce288ee3244da5d21129b0a33417f9ae2e4e
Vitaliy Busko <vitaliy.opensource@gmail.com>
2021-01-25 16:57:23 +0700

    Removed 'extern C' block from the book_parse.h

--- lib/book_parse.h
+++ lib/book_parse.h
@@ -32,30 +32,27 @@ typedef struct
     StringSize size;
 } SentenceInfo;
 
-extern "C"
-{
-    // Returns a `Book` that representes parsed text.
-    // * `p` - pointer to first byte of a text in UTF-8 encoding
-    // * `len` - length of the text in bytes.
-    Book from_utf8(unsigned char *p, unsigned int len);
+// Returns a `Book` that representes parsed text.
+// * `p` - pointer to first byte of a text in UTF-8 encoding
+// * `len` - length of the text in bytes.
+Book from_utf8(unsigned char *p, unsigned int len);
 
-    // Dispose all data. Must be called when `Book` is no longer need.
-    void dispose(Book book);
+// Dispose all data. Must be called when `Book` is no longer need.
+void dispose(Book book);
 
-    // Returns basic info of the parsed text.
-    BookInfo book_info(Book book);
+// Returns basic info of the parsed text.
+BookInfo book_info(Book book);
 
-    // Returns basic info of the parsed paragraph by its unique `index`.
-    ParagraphInfo paragraph_info(Book book, ParagraphId index);
+// Returns basic info of the parsed paragraph by its unique `index`.
+ParagraphInfo paragraph_info(Book book, ParagraphId index);
 
-    // Writes a text of the parsed paragraph by its unique `index` to `buff`.
-    // Required `buff` size can be given by calling `paragraph_info` function.
-    void paragraph_text(Book book, ParagraphId number, unsigned char *buff);
+// Writes a text of the parsed paragraph by its unique `index` to `buff`.
+// Required `buff` size can be given by calling `paragraph_info` function.
+void paragraph_text(Book book, ParagraphId number, unsigned char *buff);
 
-    // Returns basic info of the parsed sentence by its unique `index`.
-    SentenceInfo sentence_info(Book book, SentenceId index);
+// Returns basic info of the parsed sentence by its unique `index`.
+SentenceInfo sentence_info(Book book, SentenceId index);
 
-    // Writes a text of the parsed sentence by its unique `index` to `buff`.
-    // Required `buff` size can be given by calling `sentence_info` function.
-    void sentence_text(Book book, SentenceId index, unsigned char *buff);
-}
+// Writes a text of the parsed sentence by its unique `index` to `buff`.
+// Required `buff` size can be given by calling `sentence_info` function.
+void sentence_text(Book book, SentenceId index, unsigned char *buff);
